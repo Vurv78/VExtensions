9505f17532e221ee906d70d0d8c15bdb0a4f139d
Vurv78 <56230599+Vurv78@users.noreply.github.com>
2020-10-03 20:15:46 -0700

    Add default convar messages
    Tells you the default value
    Also get rid of toolcore's stuff

--- lua/autorun/server/printglobal_init_sv.lua
+++ lua/autorun/server/printglobal_init_sv.lua
@@ -2,6 +2,6 @@
 
 util.AddNetworkString("PrintGlobal_Net")
 
-CreateConVar("printglobal_charmax_sv","450",FCVAR_REPLICATED,"The amount of chars that can be sent with the e2function printGlobal()",0,2000)
-CreateConVar("printglobal_argmax_sv","100",FCVAR_REPLICATED,"The amount of arguments that can be sent with the e2function printGlobal()",0,255)
-CreateConVar("printglobal_burst_sv","4",FCVAR_REPLICATED,"How many times printGlobal can be used in a second.")
+CreateConVar("printglobal_charmax_sv","450",FCVAR_REPLICATED,"The amount of chars that can be sent with the e2function printGlobal(). Max 2000, default 450",0,2000)
+CreateConVar("printglobal_argmax_sv","100",FCVAR_REPLICATED,"The amount of arguments that can be sent with the e2function printGlobal(). Max 255, default 100",0,255)
+CreateConVar("printglobal_burst_sv","4",FCVAR_REPLICATED,"How many times printGlobal can be used in a second. Default 4 times per second, same as default print")
--- lua/weapons/gmod_tool/stools/e2toolcore_sh.lua
+++ lua/weapons/gmod_tool/stools/e2toolcore_sh.lua
@@ -4,17 +4,6 @@
   / __/  __/ /     / / / __ \/ __ \/ /  / /   / __ \/ ___/ _ \
  / /___ / __/     / / / /_/ / /_/ / /  / /___/ /_/ / /  /  __/
 /_____//____/    /_/  \____/\____/_/   \____/\____/_/   \___/ 
-    Allows E2 to run on use of a custom tool
+    Pranked!
+        This does nothing... for now..
 ]]
-
-TOOL.Category		= "E2 Tool"
-TOOL.Name			= "#E2Tool"
-TOOL.Command		= nil
-TOOL.ConfigName		= ""
-
-if SERVER then
-    -- Server
-
-else
-    -- Client
-end
