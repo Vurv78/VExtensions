bf1394eb32f7aca75c4e03a7c097030eae3c6cb5
Vurv78 <56230599+Vurv78@users.noreply.github.com>
2020-10-03 20:13:59 -0700

    More lenient default convars
    Allows you to use twice as many args and 200 more characters. (Note: Can't edit the convar to go beyond 255 without editing the source code to use more bits in net messages)

--- lua/autorun/server/printglobal_init_sv.lua
+++ lua/autorun/server/printglobal_init_sv.lua
@@ -2,6 +2,6 @@
 
 util.AddNetworkString("PrintGlobal_Net")
 
-CreateConVar("printglobal_charmax_sv","350",FCVAR_REPLICATED,"The amount of chars that can be sent with the e2function printGlobal()",0,2000)
-CreateConVar("printglobal_argmax_sv","50",FCVAR_REPLICATED,"The amount of arguments that can be sent with the e2function printGlobal()",0,255)
+CreateConVar("printglobal_charmax_sv","450",FCVAR_REPLICATED,"The amount of chars that can be sent with the e2function printGlobal()",0,2000)
+CreateConVar("printglobal_argmax_sv","100",FCVAR_REPLICATED,"The amount of arguments that can be sent with the e2function printGlobal()",0,255)
 CreateConVar("printglobal_burst_sv","4",FCVAR_REPLICATED,"How many times printGlobal can be used in a second.")
